820dddbbb170693808bff4c384fd17390a68badc
jsalaz1989 <jsalaz1989@gmail.com>
2021-12-01 21:17:31 +0100

    New django-upload wheel

--- apps/django-upload-2/upload/models.py
+++ apps/django-upload-2/upload/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 
-from hello_django.storage_backends import PublicMediaStorage, PrivateMediaStorage
+from server.storage_backends import PublicMediaStorage, PrivateMediaStorage
 
 
 class Upload(models.Model):
     uploaded_at = models.DateTimeField(auto_now_add=True)
-    file = models.FileField(storage=PublicMediaStorage())
+    file = models.FileField()
 
 
 class UploadPrivate(models.Model):

c4ab67c2fa0c99dda45c4d4a576d7f3b528e5cf2
jsalaz1989 <jsalaz1989@gmail.com>
2021-12-06 19:48:42 +0100

    Trying TEST key in DATABASES dict in settings.py

--- image/app/server/settings.py
+++ image/app/server/settings.py
@@ -100,6 +100,9 @@ DATABASES = {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
         "NAME": os.environ.get("SQL_DATABASE", os.path.join(BASE_DIR, "db.sqlite3")),
         "USER": os.environ.get("SQL_USER", "user"),
+        'TEST': {
+             'MIRROR': 'default',
+        },
         "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
         "PORT": os.environ.get("SQL_PORT", "5432"),

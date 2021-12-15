8b3038c26665607b01a2f175fade5e0ff1ab5a7c
jsalaz1989 <jsalaz1989@gmail.com>
2021-12-15 19:38:53 +0100

    Not sure why can't delete mediafiles

---
+++ image/app/entrypoint.dev.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py flush --no-input
+python manage.py makemigrations
+python manage.py migrate
+
+# poetry env info
+ls -a .venv/lib/python3.10/site-packages
+ls -a tests
+
+
+exec "$@"

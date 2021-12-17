de54c86be58cf0848a734a21087b6b32d45a0cff
jsalaz1989 <jsalaz1989@gmail.com>
2021-12-17 23:25:57 +0100

    Trying test in entrypoint

--- image/app/entrypoint.prod.sh
+++ image/app/entrypoint.prod.sh
@@ -11,15 +11,8 @@ then
     echo "PostgreSQL started"
 fi
 
-# echo $PWD
-# ls -a $PWD
-# ls -a home/app/web
-# . .venv/bin/activate
 which python
-# ls -a .venv/lib/python3.10/site-packages
-# ls -la .venv/bin
-# ls -a ./server
-# gunicorn --version
+ls -a .
 
 gunicorn server.wsgi:application --bind 0.0.0.0:$PORT --reload --log-level debug --access-logfile - --error-logfile -
 
